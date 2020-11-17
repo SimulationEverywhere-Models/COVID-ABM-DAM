cb09eb21ce42d8707945ac2129359b802cfc8b43
mibo99 <72056254+mibo99@users.noreply.github.com>
2020-11-17 10:07:45 -0500

    Update roomfilter.cpp

--- roomfilter.cpp
+++ roomfilter.cpp
@@ -11,63 +11,95 @@ using namespace std;
 
 //Port definition
 //definition of input/output events 
-struct Room_Specs_Filter{
-	struct room_in : public in_port</*float?*/>{};
-	struct room_out : public out_port<> {};
+struct Room_Specs_Filter_Ports{
+	struct room_in : public in_port<float/>{};
+	struct room_out : public out_port<float> {};
 };
 
 struct room_in{
-	int PersonID;
+	long PersonID;
 	int people_in_room;
 	float room_size;
 	float viral_particles;
-	int RoomID;
+	long RoomID;
 };
 struct room_out{
-	int PersonID;
+	long PersonID;
 	int people_in_room;
 	float room_size;
 	float viral_particles;
-	int RoomID;
+	long RoomID;
 };
 
 
-template<typename TIME>
+template<typename TIME> class Room_Specs_Filter{
+	TIME UpdateTime;
 
-class Room_Specs_Filter{
-	public:
-		using input_ports=tuple<typename //unfinished
-		using output_ports=tuple<typename //"
+	Room_Specs_Filter () noexcept{
+    	UpdateTime = TIME("00:00:05:00");
+    	state.next_internal = std:numeric_limits<TIME>::infinity();
+    	state.model_active = false;
+	}
+}
+    
+//state definition 
+struct state_type{
+	long PersonID;
+	int people_in_room;
+	float room_size;
+	float viral_particles;
+	long RoomID;
+	
+	bool model_active;
+	bool next_internal;
 };
 
 //contains all messages passing the filter 
 struct msgs_passing_filter{
-	
+	vector<> messages;
 };
 
+	state_type state;
+	using input_ports::tuple<typename Room_Specs_Filter_Ports::room_in>;
+	using output_ports::tuple<typename Room_Specs_Filter_Ports::room_out>;
 
 //external transition function
-void external_transition(TIME, ...){
-	if (x.PersonID == ID) 		//questionable
-	msgs_passing_filter = x + msg_passing_filter;
+void external_transition(TIME, typename make_message_bags<input_ports>::type mbs){
+	if (state.PersonID == ID) {
+	msgs_passing_filter.messages.insert(people_in_room);
+	msgs_passing_filter.messages.insert(room_size);
+	msgs_passing_filter.messages.insert(viral_particles);
+	msgs_passing_filter.messages.insert(RoomID);
+	}
 }
 
 //internal transition function
 void internal_transition(){
 	//clear message passing through the filter 
-	msgs_passing_filter.clear;
+	msgs_passing_filter.messages.clear;
 }
 
+//time advance function
 TIME time_advance() const{
-	//needs fixing
 	TIME next_internal;
-	if msgs_passing_filter = numeric_limits<TIME>::infinity();
-	else = 0;
+	if (msgs_passing_filter.active)
+		next_internal = numeric_limits<TIME>::infinity();
+	else {
+		next_internal = 0;
+	}
 	return next_internal;
 }
 
 //output function
-{msgs_passing_filter}
+typename make_message_bags<output_ports>::type output() const{
+ 	typename make_message_bags<output_ports>::type bags;
+ 
+  	if (msgs_passing_filter.messages.empty() == False){
+  		get_messages<typename Room_Specs_Filter_Ports::room_out(bags).push_back(msgs_passing_filter); 
+  	}
+
+ return bags;
+}
 
 
 
