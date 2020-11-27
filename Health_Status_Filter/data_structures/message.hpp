d829a8fc86821917d3bab5a0ce467633df7da301
seanaysmith <71235346+seanaysmith@users.noreply.github.com>
2020-11-27 12:08:49 -0500

    Add files via upload

---
+++ Health_Status_Filter/data_structures/message.hpp
@@ -0,0 +1,28 @@
+#ifndef BOOST_SIMULATION_MESSAGE_HPP
+#define BOOST_SIMULATION_MESSAGE_HPP
+
+#include <assert.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*******************************************/
+/**************** health_status ****************/
+/*******************************************/
+struct health_status{
+    health_status(){}
+    health_status(long i_person_id, bool i_issick)
+   :Person_ID(i_person_id), IsSick(i_issick){}
+    
+    long Person_ID; 
+    bool IsSick;
+    
+};
+
+istream& operator>> (istream& is, health_status& msg);
+
+ostream& operator<<(ostream& os, const health_status& msg);
+
+
+#endif // BOOST_SIMULATION_MESSAGE_HPP
