d175da450161513a8596f6f9525529e8c6e12ded
mibo99 <72056254+mibo99@users.noreply.github.com>
2020-11-28 18:13:01 -0500

    Update message.hpp

--- Room_Specs_Filter/message.hpp
+++ Room_Specs_Filter/message.hpp
@@ -7,10 +7,11 @@
 
 using namespace std;
 
-struct room_in{
+struct room_specs_in{
 room_specs_in(){}     //default constructor of "garbage"
 room_specs_in(long i_person_id, int i_people_in_room, float i_room_size, float i_viral_particles, long i_room_ID)
-    :Person_ID(i_person_ID), people_in_room(i_people_in_room), room_size(i_room_size), viral_particles(i_viral_particles), room_ID(i_room_ID){}
+    :Person_ID(i_person_id), people_in_room(i_people_in_room), room_size(i_room_size), viral_particles(i_viral_particles), room_ID(i_room_ID){}
+
     long Person_ID;
 	int people_in_room;
 	float room_size;
@@ -19,10 +20,10 @@ room_specs_in(long i_person_id, int i_people_in_room, float i_room_size, float i
 };
 
 //include??
-struct room_out{
+struct room_specs_out{
 room_specs_out(){}
 room_specs_out(long i_person_id, int i_people_in_room, float i_room_size, float i_viral_particles, long i_room_ID)
-    :Person_ID(i_person_ID), people_in_room(i_people_in_room), room_size(i_room_size), viral_particles(i_viral_particles), room_ID(i_room_ID){}
+    :Person_ID(i_person_id), people_in_room(i_people_in_room), room_size(i_room_size), viral_particles(i_viral_particles), room_ID(i_room_ID){}
     long Person_ID;
 	int people_in_room;
 	float room_size;
@@ -31,6 +32,6 @@ room_specs_out(long i_person_id, int i_people_in_room, float i_room_size, float 
 }
 
 istream& operator>> (istream& is, room_specs_in& msg);
-ostream& operator<< (ostream& os, const room_specs_out& msg);
+ostream& operator<< (ostream& os, room_specs_out& msg);
 
 #endif
